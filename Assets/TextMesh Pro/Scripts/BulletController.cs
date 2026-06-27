23c10ae02a55d1e4a81bcc63f3f492cad5ba1c94
afrid-zaman <129637027+Yassin2626@users.noreply.github.com>
2026-06-27 15:36:39 -0400

    update

--- Assets/TextMesh Pro/Scripts/BulletController.cs
+++ Assets/TextMesh Pro/Scripts/BulletController.cs
@@ -7,7 +7,7 @@ public class BulletController : MonoBehaviour {
 
     public float delayBeforeShrink = 2f;
     public float shrinkDuration = 1f;
-    public int speed = 25;
+    public int speed = 40;
 
     public string shooterId = "";
     public int damage = 10;
